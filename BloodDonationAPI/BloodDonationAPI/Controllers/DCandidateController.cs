c0bddce88f8f23ff546d5de2388a89016a8b03c6
Siddhant Ashok <siddhant.ashok091994@gmail.com>
2024-12-15 22:24:58 +0530

    inital commit with Blood donation API

---
+++ BloodDonationAPI/BloodDonationAPI/Controllers/DCandidateController.cs
@@ -0,0 +1,110 @@
+﻿using BloodDonationAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BloodDonationAPI.Controllers
+{
+    [Route("api/DCandidate")]
+    [ApiController]
+    public class DCandidateController : ControllerBase
+    {
+
+        private readonly DonationDBContext _context;
+
+        public DCandidateController(DonationDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/DCandidate
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<DCandidate>>> GetDCandidates()
+        {
+            return await _context.DCandidates.ToListAsync();
+        }
+
+        // GET api/DCandidate/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<DCandidate>> GetDCandidate(int id)
+        {
+            //return await _context.DCandidates.Where(c => c.id == id).FirstOrDefaultAsync();
+
+            var dCandidate = await _context.DCandidates.FindAsync(id);
+
+            if (dCandidate == null)
+            {
+                return NotFound();
+            }
+
+            return dCandidate;
+        }
+
+        // POST api/DCandidate
+        [HttpPost]
+        public async Task<ActionResult<DCandidate>> Post([FromBody] DCandidate dCandidate)
+        {
+            await _context.DCandidates.AddAsync(dCandidate);
+            await _context.SaveChangesAsync();
+            //awiat _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetDCandidate", new { id = dCandidate.id }, dCandidate);
+
+        }
+
+        // PUT api/DCandidate/5
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Put(int id, [FromBody] DCandidate dCandidate)
+        {
+            dCandidate.id = id;
+
+            _context.Entry(dCandidate).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DCandidateExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // DELETE api/DCandidate/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<DCandidate>> Delete(int id)
+        {
+
+            var dCandidate = await _context.DCandidates.FindAsync(id);
+            if (dCandidate != null)
+            {
+                _context.DCandidates.Remove(dCandidate);
+                await _context.SaveChangesAsync();
+                return dCandidate;
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        private bool DCandidateExists(int id)
+        {
+            return _context.DCandidates.Any(c => c.id == id);
+        }
+    }
+}
---
+++ BloodDonationAPI/BloodDonationAPI/Startup.cs
@@ -0,0 +1,66 @@
+using BloodDonationAPI.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BloodDonationAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+
+            services.AddDbContext<DonationDBContext>(options => 
+            options.UseSqlServer(Configuration.GetConnectionString("DevConnection"))
+            );
+
+            services.AddMvc(options => options.EnableEndpointRouting = false);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            //app.UseStaticFiles();
+
+            app.UseRouting();
+
+            //app.UseAuthorization();
+
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapRazorPages();
+            //});
+
+            app.UseMvc(routeBuilder =>
+            {
+                //routeBuilder.MapRoute("default", "{controller=Home}/{action=Index}/{id?}");
+                routeBuilder.MapRoute("default", "{controller=Home}/{action=Index}");
+            });
+        }
+    }
+}
